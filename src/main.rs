8a73ae78ca62184d410af113e8e799f21a085b24
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-02-17 15:13:10 +0500

    slice function

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 mod Ch01;
-use crate::Ch01::integer;
-use crate::Ch01::floating;
+use crate::Ch01::{integer, floating};
+mod Ch02;
+use crate::Ch02::slice;
 
 fn main() {
+    let str = String::from("lol me");
     println!("Hello, world!");
     println!("{}", integer::add_i(1,2));
     println!("{}", integer::subtract_i(1,2));
     println!("{}", integer::multiply_i(1,2));
     println!("{}", integer::divide_i(1,2));
     println!("{}", floating::add_fl(1.2,2.2));
+    println!("{}", slice::first_word(&str));
 }
